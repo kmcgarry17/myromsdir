08d959995ed3285304d1fbdfe44b668216bbf07a
kmcgarry17 <kelly.m.mcgarry@uconn.edu>
2020-11-12 11:30:18 -0500

    original roms a8cdf6 code

---
+++ roms/ROMS/Fish/fish_var.h
@@ -0,0 +1,138 @@
+              CASE ('idFwwt')
+                idFwwt=varid
+              CASE ('idFworth')
+                idFworth=varid
+              CASE ('idFlngth')
+                idFlngth=varid
+              CASE ('idFpval')
+                idFpval=varid
+              CASE ('idFage')
+                idFage=varid
+              CASE ('idFbday')
+                idFbday=varid
+              CASE ('idFyield')
+                idFyield=varid
+              CASE ('idFcsmPS')
+                idFcsmPS=varid
+              CASE ('idFcsmPL')
+                idFcsmPL=varid
+              CASE ('idFcsmZS')
+                idFcsmZS=varid
+              CASE ('idFcsmZL')
+                idFcsmZL=varid
+              CASE ('idFcsmZP')
+                idFcsmZP=varid
+              CASE ('idFcsmF1')
+                idFcsmF1=varid
+              CASE ('idFcsmF2')
+                idFcsmF2=varid
+              CASE ('idFcsmF3')
+                idFcsmF3=varid
+              CASE ('idFcsmF4')
+                idFcsmF4=varid
+              CASE ('idFcsmF5')
+                idFcsmF5=varid
+              CASE ('idFresp')
+                idFresp=varid
+              CASE ('idFdegg')
+                idFdegg=varid
+              CASE ('idFnegg')
+                idFnegg=varid
+              CASE ('idFdysac')
+                idFdysac=varid
+              CASE ('idFnysac')
+                idFnysac=varid
+              CASE ('idFdlarv')
+                idFdlarv=varid
+              CASE ('idFnlarv')
+                idFnlarv=varid
+              CASE ('idFdjuv')
+                idFdjuv=varid
+              CASE ('idFnjuv')
+                idFnjuv=varid
+              CASE ('idFnsuba')
+                idFnsuba=varid
+              CASE ('idFmortN')
+                idFmortN=varid
+              CASE ('idFmortS')
+                idFmortS=varid
+              CASE ('idFmortF')
+                idFmortF=varid
+              CASE ('idFmortP')
+                idFmortP=varid
+              CASE ('idFmrtPs')
+                idFmrtPs=varid
+              CASE ('idFbatch')
+                idFbatch=varid
+              CASE ('idFeggs')
+                idFeggs=varid
+              CASE ('idSpwnloc')
+                idSpwnloc=varid
+              CASE ('idFspc')
+                idFspc=varid
+              CASE ('idFlfsg')
+                idFlfsg=varid
+              CASE ('idFdeath')
+                idFdeath=varid
+              CASE ('idFhbhv')
+                idFhbhv=varid
+              CASE ('idFvbhv')
+                idFvbhv=varid
+              CASE ('idFalv')
+                idFalv=varid
+              CASE ('idFTspwn')
+                idFTspwn=varid
+              CASE ('idFiniw')
+                idFiniw=varid
+# ifdef PREDATOR
+              CASE ('idPwwt')
+                idPwwt=varid
+              CASE ('idPworth')
+                idPworth=varid
+              CASE ('idPpval')
+                idPpval=varid
+              CASE ('idPcsmF1')
+                idPcsmF1=varid
+              CASE ('idPcsmF2')
+                idPcsmF2=varid
+              CASE ('idPcsmF3')
+                idPcsmF3=varid
+              CASE ('idPcsmF4')
+                idPcsmF4=varid
+              CASE ('idPcsmF5')
+                idPcsmF5=varid
+              CASE ('idPspc')
+                idPspc=varid
+              CASE ('idPalv')
+                idPalv=varid
+# endif
+# ifdef FISHING_FLEET
+              CASE ('idBxprt')
+                idBxprt=varid
+              CASE ('idByprt')
+                idByprt=varid
+              CASE ('idBnprt')
+                idBnprt=varid
+              CASE ('idBnfloc')
+                idBnfloc=varid
+              CASE ('idBxfsh1')
+                idBxfsh1=varid
+              CASE ('idByfsh1')
+                idByfsh1=varid
+              CASE ('idBxfsh2')
+                idBxfsh2=varid
+              CASE ('idByfsh2')
+                idByfsh2=varid
+              CASE ('idBcatch')
+                idBcatch=varid
+              CASE ('idBcmctch')
+                idBcmctch=varid
+              CASE ('idBacpue')
+                idBacpue=varid
+              CASE ('idBpcpue')
+                idBpcpue=varid
+              CASE ('idBpdist')
+                idBpdist=varid
+              CASE ('idBpnear')
+                idBpnear=varid
+# endif
